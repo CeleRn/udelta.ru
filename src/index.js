0adbb54e012657bd56367e0c9465a201cf2b7298
CeleRn <celern84@gmail.com>
2018-04-15 02:48:55 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+require('./styles/main.scss');
+require("./images/logo-udelta.svg");
+
+
+//////////////
+// Favicons //
+//////////////
+
+require('./images/favicons/android-icon-36x36.png');
+require('./images/favicons/android-icon-48x48.png');
+require('./images/favicons/android-icon-72x72.png');
+require('./images/favicons/android-icon-96x96.png');
+require('./images/favicons/android-icon-144x144.png');
+require('./images/favicons/android-icon-192x192.png');
+require('./images/favicons/apple-icon.png');
+require('./images/favicons/apple-icon-57x57.png');
+require('./images/favicons/apple-icon-60x60.png');
+require('./images/favicons/apple-icon-72x72.png');
+require('./images/favicons/apple-icon-76x76.png');
+require('./images/favicons/apple-icon-114x114.png');
+require('./images/favicons/apple-icon-120x120.png');
+require('./images/favicons/apple-icon-144x144.png');
+require('./images/favicons/apple-icon-152x152.png');
+require('./images/favicons/apple-icon-180x180.png');
+require('./images/favicons/apple-icon-precomposed.png');
+require('./images/favicons/browserconfig.xml');
+require('./images/favicons/favicon.ico');
+require('./images/favicons/favicon-16x16.png');
+require('./images/favicons/favicon-32x32.png');
+require('./images/favicons/favicon-96x96.png');
+// require('./images/favicons/manifest.json');
+require('./images/favicons/ms-icon-70x70.png');
+require('./images/favicons/ms-icon-144x144.png');
+require('./images/favicons/ms-icon-150x150.png');
+require('./images/favicons/ms-icon-310x310.png');
